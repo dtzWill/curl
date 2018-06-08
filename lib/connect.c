99525f234fb416c9ea9f002506c8129726cf1ae1
Will Dietz <w@wdtz.org>
2018-06-07 22:51:16 -0500

    connisalive: handle borked handle pointers

--- lib/connect.c
+++ lib/connect.c
@@ -1273,6 +1273,13 @@ curl_socket_t Curl_getconnectinfo(struct Curl_easy *data,
  */
 bool Curl_connalive(struct connectdata *conn)
 {
+  /* kludge around use-after-free */
+  if (!conn->data || !GOOD_EASY_HANDLE(conn->data)
+      || conn->data->easy_conn != conn) {
+    //infof(
+    return false;
+  }
+
   /* First determine if ssl */
   if(conn->ssl[FIRSTSOCKET].use) {
     /* use the SSL context */
